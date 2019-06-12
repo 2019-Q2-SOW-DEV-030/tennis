a9d712e66e66b02d98196f6f65198fd895b92cf2
2019-Q2-SOW-DEV-030 <2019.q2.sow.dev.030@gmail.com>
2019-06-12 06:41:38 +0200

    Refactor: Extract decide winner logics into functions

--- src/TennisGame.js
+++ src/TennisGame.js
@@ -9,10 +9,19 @@ var TennisGame = function () {
 
     var playerOnePoints = constants.STRING_ZERO;
     var playerTwoPoints = constants.STRING_ZERO;
+    var DEUCE_POINT = constants.NUMBER_THREE;
+    var ADVANTAGE_POINT = constants.NUMBER_FOUR;
     var POINTS_TO_SCORE = {
         1: "15",
         2: "30",
         3: "40"
+    };
+    var RESULTS = {
+        DEUCE: "Deuce",
+        PLAYER_ONE_WINS: "Player1 wins.",
+        PLAYER_TWO_WINS: "Player2 wins.",
+        PLAYER_ONE_ADVANTAGE: "Player1 gets advantage",
+        PLAYER_TWO_ADVANTAGE: "Player2 gets advantage",
     };
     var deuceGame = false;
     var advantagePlayer;
@@ -42,24 +51,24 @@ var TennisGame = function () {
     }
 
     function decideWinner() {
-        if (!deuceGame) {
+        if (!isDeuceGame()) {
             if (isDeucePoints()) {
-                deuceGame = true;
-                scoreBoard.result = "Deuce";
-            } else if (playerOnePoints > constants.NUMBER_THREE && playerOnePoints > playerTwoPoints) {
-                scoreBoard.result = "Player1 wins.";
-            } else if (playerTwoPoints > constants.NUMBER_THREE && playerTwoPoints > playerOnePoints) {
-                scoreBoard.result = "Player2 wins.";
+                setThisGameAsDeuce();
+                setResultAsDeuce();
+            } else if (isPlayerWinsTheGame(playerOnePoints, playerTwoPoints)) {
+                setResultAsPlayerOneWinsTheGame();
+            } else if (isPlayerWinsTheGame(playerTwoPoints, playerOnePoints)) {
+                setResultAsPlayerTwoWinsTheGame();
             }
         } else {
-            if (playerOnePoints === constants.NUMBER_FOUR && playerOnePoints === playerTwoPoints) {
-                playerOnePoints--;
-                playerTwoPoints--;
-                scoreBoard.result = "Deuce";
+            if (isBothPlayersInAdvantage()) {
+                setPointsBackToDeuce();
+                setResultAsDeuce();
+                setAdvantagePlayerAsEmpty();
             } else if (advantagePlayer === "player1") {
-                scoreBoard.result = "Player1 gets advantage";
+                setResultAsPlayerOneAdvantage();
             } else if (advantagePlayer === "player2") {
-                scoreBoard.result = "Player2 gets advantage";
+                setResultAsPlayerTwoAdvantage();
             }
         }
     }
@@ -73,7 +82,7 @@ var TennisGame = function () {
     }
 
     function setAdvantagePlayer(player) {
-        if(isDeuceGame()) {
+        if (isDeuceGame()) {
             advantagePlayer = player;
         }
     }
@@ -82,14 +91,64 @@ var TennisGame = function () {
         return deuceGame;
     }
 
+    function setThisGameAsDeuce() {
+        deuceGame = true;
+    }
+
+    function setResultAsDeuce() {
+        setResult(RESULTS.DEUCE);
+    }
+
     function isDeucePoints() {
         return (playerOnePoints === 3 && playerOnePoints === playerTwoPoints);
+    }
+
+    function isPlayerWinsTheGame(point1, point2) {
+        return point1 > DEUCE_POINT && point1 > point2;
+    }
+
+    function setResultAsPlayerOneWinsTheGame() {
+        setResult(RESULTS.PLAYER_ONE_WINS);
+    }
+
+    function setResultAsPlayerTwoWinsTheGame() {
+        setResult(RESULTS.PLAYER_TWO_WINS);
+    }
+
+
+    function isBothPlayersInAdvantage() {
+        return isPointsAreEqual(ADVANTAGE_POINT);
+    }
+
+    function setAdvantagePlayerAsEmpty() {
+        advantagePlayer = "";
+    }
+
+    function setResultAsPlayerOneAdvantage() {
+        setResult(RESULTS.PLAYER_ONE_ADVANTAGE);
+    }
+
+    function setResultAsPlayerTwoAdvantage() {
+        setResult(RESULTS.PLAYER_TWO_ADVANTAGE);
+    }
+
+    function setPointsBackToDeuce() {
+        playerOnePoints--;
+        playerTwoPoints--;
     }
 
     function setScore(score, player) {
         if (score) {
             scoreBoard[player] = score;
         }
+    }
+
+    function setResult(value) {
+        scoreBoard.result = value;
+    }
+
+    function isPointsAreEqual(pointsToCheck) {
+        return (playerOnePoints === pointsToCheck && playerOnePoints === playerTwoPoints);
     }
 
     function pointsToScore(points) {
