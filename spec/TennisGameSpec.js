66c88276cd9b31eb3dca0af570d6d012b12a55ac
2019-Q2-SOW-DEV-030 <2019.q2.sow.dev.030@gmail.com>
2019-06-12 05:38:19 +0200

    Handle player1 scores first two balls

--- spec/TennisGameSpec.js
+++ spec/TennisGameSpec.js
@@ -3,8 +3,8 @@ var TennisGame = require("../src/TennisGame");
 describe('TennisGame', function () {
     var tennisGame;
 
-    beforeEach(function() {
-        tennisGame= new TennisGame();
+    beforeEach(function () {
+        tennisGame = new TennisGame();
     });
 
     it('Should get the scores of the players at match start', function () {
@@ -25,7 +25,7 @@ describe('TennisGame', function () {
         expect(score.player2).toEqual("0");
     });
 
-    it('Should player2 win the first ball', function() {
+    it('Should player2 win the first ball', function () {
 
         tennisGame.playerTwoScored();
 
@@ -33,5 +33,16 @@ describe('TennisGame', function () {
 
         expect(score.player1).toEqual("0");
         expect(score.player2).toEqual("15");
+    });
+
+    it('Should player1 score first two balls', function () {
+
+        tennisGame.playerOneScored();
+        tennisGame.playerOneScored();
+
+        var score = tennisGame.getScore();
+
+        expect(score.player1).toEqual("30");
+        expect(score.player2).toEqual("0");
     });
 })
--- src/TennisGame.js
+++ src/TennisGame.js
@@ -1,21 +1,29 @@
 var constants = require("../src/utils/constants");
 
-var TennisGame = function() {
+var TennisGame = function () {
 
     var scoreBoard = {
         player1: constants.STRING_ZERO,
         player2: constants.STRING_ZERO
     };
 
+    var playerOnePoints = 0;
+
     this.playerOneScored = function () {
-        scoreBoard.player1 = constants.STRING_FIFTEEN;
+        playerOnePoints++;
+
+        if (playerOnePoints === 1) {
+            scoreBoard.player1 = "15";
+        } else if (playerOnePoints === 2) {
+            scoreBoard.player1 = "30"
+        }
     }
 
-    this.playerTwoScored = function() {
+    this.playerTwoScored = function () {
         scoreBoard.player2 = constants.STRING_FIFTEEN;
     }
 
-    this.getScore = function() {
+    this.getScore = function () {
         return scoreBoard;
     }
 }
